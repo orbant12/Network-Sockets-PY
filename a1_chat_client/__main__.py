f7c21a7367828b357ada9c522fa14fcb9dc504e4
orbant12 <orbant1@gmail.com>
2025-04-01 17:50:10 +0200

    updates made

--- a1_chat_client/__main__.py
+++ a1_chat_client/__main__.py
@@ -38,50 +38,59 @@ def send(string_bytes,sock):
         return data.decode("utf-8")
 
 def messaging(sock):
-    userInput = input()
+    while True:
+        userInput = input()
 
-    handleInput = userInput.split(" ", 1)
-
-    if len(handleInput) == 2:
-        dest_user, message = handleInput
-        dest_user = dest_user.removeprefix('@')
-        string_bytes_message = f"SEND {dest_user} {message}\n".encode("utf-8")
-
-        response_mess_sent = send(string_bytes_message, sock)
-
-        # Success SENT
-        if response_mess_sent == "SEND-OK\n":
-            print("Sent")
-
-        elif response_mess_sent == "BAD-DEST-USER\n":
-            print("The destination user does not exist")
-
-        else:
-            print("Incorrect input !")
-    elif len(handleInput) == 1:
-        if userInput == "!who":
+        if userInput == "!quit":
+            return
+        elif userInput == "!who":
             users = getonline(sock)
             if users != False:
                 print(f'There are {len(users)} online users:')
                 for user in users:
-                    print(user + "\n")
+                    print(user)
 
         else:
-            print("Incorrect input !")
-    else:
-        print("Incorrect input !")
 
-    # Different Thread
+            handleInput = userInput.split(" ", 1)
+
+            if(len(handleInput)):
+                dest_user, message = handleInput
+                dest_user = dest_user.removeprefix('@')
+                string_bytes_message = f"SEND {dest_user} {message}\n".encode("utf-8")
+
+                response_mess_sent = send(string_bytes_message, sock)
+
+                # Success SENT
+                if response_mess_sent == "SEND-OK\n":
+                    print("The message was sent successfully")
+                elif response_mess_sent == "BAD-DEST-USER\n":
+                    print("The destination user does not exist")
+                elif response_mess_sent == "BAD-RQST-HDR\n":
+                    print("Error: Unknown issue in previous message header.")
+                elif response_mess_sent == "BAD-RQST-BODY\n":
+                    print("Error: Unknown issue in previous message body.")
+
+            else:
+                print("Incorrect input !")
 
 def recieving(sock):
     while True:
-        if not False:
-            rdlist, wrlist, exlist = select.select([sock], [], [])
-            for client in rdlist:
-                message = client.recv(4096).decode("utf-8")
-                print(message)
-        else:
-            break
+        rdlist, wrlist, exlist = select.select([sock], [], [])
+        for client in rdlist:
+            message = client.recv(4096).decode("utf-8")
+            if message.startswith("DELIVERY"):
+                # Extract the sender and content from DELIVERY <name> <msg>
+                parts = message.split(" ", 2)
+                if len(parts) >= 3:
+                    sender = parts[1]
+                    content = parts[2].strip()
+                    print(f"From {sender}: {content}")
+            elif message == "BAD-RQST-HDR\n":
+                print("Error: Unknown issue in previous message header.")
+            elif message == "BAD-RQST-BODY\n":
+                print("Error: Unknown issue in previous message body.")
+
 
 def getonline(sock):
     users = []
@@ -93,6 +102,15 @@ def getonline(sock):
         users_str = response_all_users[8:].strip()
         users = users_str.split(",")
         return users
+
+    elif response_all_users == "BAD-RQST-HDR\n":
+        print("Error: Unknown issue in previous message header.")
+        return False
+
+    elif response_all_users == "BAD-RQST-BODY\n":
+        print("Error: Unknown issue in previous message body.")
+        return False
+
     else:
         return False
 
@@ -111,37 +129,60 @@ def main() -> None:
 
     # 1.)
     print("Welcome to Chat Client. Enter your login:")
-    username = input()
+    authed = False
+    while not authed:
 
-    for letter in username:
-        if letter in malformed:
-            print(f"Cannot log in as {username}. That username contains disallowed characters.")
-            return
+        username = input()
 
-    # Send Username to server
-    string_bytes_2 = f"HELLO-FROM {username}\n".encode("utf-8")
+        if username == "!quit":
+            break
+        else:
+            found = False
+            for letter in username:
+                if letter in malformed:
+                    print(f"Cannot log in as {username}. That username contains disallowed characters.")
+                    found = True
 
-    response_login_attempt = send(string_bytes_2,sock)
+            if not found:
 
-    if not response_login_attempt:
-        print("Socket is closed")
+                # Send Username to server
+                string_bytes_2 = f"HELLO-FROM {username}\n".encode("utf-8")
 
-    elif response_login_attempt == "IN-USE\n":
-        print("Cannot log in as <username>. That username is already in use.")
+                response_login_attempt = send(string_bytes_2,sock)
 
-    elif response_login_attempt == "BUSY\n":
-        print("Cannot log in. The server is full!")
+                if not response_login_attempt:
+                    print("Socket is closed")
 
-    #Success LOGIN
-    elif response_login_attempt == f"HELLO {username}\n":
-        print(f"Successfully logged in as {username}")
-        t = threading.Thread(target=messaging, args=(sock,))
-        t_2 = threading.Thread(target=recieving, args=(sock,))
+                elif response_login_attempt == "IN-USE\n":
+                    print("Cannot log in as <username>. That username is already in use. \n")
 
-        t.start()
-        t_2.start()
+                elif response_login_attempt == "BUSY\n":
+                    print("Cannot log in. The server is full! \n")
+
+                elif response_login_attempt == "BAD-RQST-HDR\n":
+                    print("Error: Unknown issue in previous message header.")
+
+                elif response_login_attempt == "BAD-RQST-BODY\n":
+                    print("Error: Unknown issue in previous message body.")
+
+                #Success LOGIN
+                elif response_login_attempt == f"HELLO {username}\n":
+                    print(f"Successfully logged in as {username}")
+                    authed = True
+                    t = threading.Thread(target=messaging, args=(sock,), daemon=False)
+                    t_2 = threading.Thread(target=recieving, args=(sock,), daemon=True)
+
+                    t.start()
+                    t_2.start()
+
+                    t.join()
+                    t_2.join()
+                    break
 
 
+                    # Treading issue
+
+    return
 
 
 if __name__ == "__main__":
